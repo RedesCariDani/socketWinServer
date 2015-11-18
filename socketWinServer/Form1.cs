8d0a9c45f58388b1b5a27b6c4567e5bbe987be2b
Daniel Peralta Madriz <danielperaltamadriz@gmail.com>
2015-11-18 00:06:56 -0600

    ,,,

--- socketWinServer/Form1.cs
+++ socketWinServer/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.Sockets;
 using System.Net;
+using System.Threading;
 
 namespace socketWinServer
 {
@@ -31,49 +32,55 @@ namespace socketWinServer
             IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());           
             IPAddress ipAddress = ipHostInfo.AddressList[0];
             IPEndPoint localEndPoint = new IPEndPoint(ipAddress, int.Parse(puerto.Text));
-            textBox1.Text = "Conectándose a: " + localEndPoint.ToString();
+            //puerto.Text = "Conectándose a: " + localEndPoint.ToString();
 
             // Create a TCP/IP socket.
-            Socket listener = new Socket(AddressFamily.InterNetwork,
+            Socket server = new Socket(AddressFamily.InterNetwork,
                 SocketType.Stream, ProtocolType.Tcp);
 
             // Bind the socket to the local endpoint and 
             // listen for incoming connections.
             try
             {
-                listener.Bind(localEndPoint);
-                listener.Listen(10);
+                server.Bind(localEndPoint);
+                server.Listen(10);
 
                 // Start listening for connections.
-                while (true)
-                {
-                    textBox1.Text ="Waiting for a connection...";
+                //while (true)
+                //{
+                    //textBox1.Text ="Waiting for a connection...";
                     // Program is suspended while waiting for an incoming connection.
-                    Socket handler = listener.Accept();
+                    Socket handler = server.Accept();
                     data = null;
+                Console.WriteLine("Client has been accepted!");
+                var thread = new Thread(() => ClientSession(handler))
+                {
+                    IsBackground = true
+                };
+                thread.Start();
 
-                    // An incoming connection needs to be processed.
-                    while (true)
-                    {
-                        bytes = new byte[1024];
-                        int bytesRec = handler.Receive(bytes);
-                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                        if (data.IndexOf("<EOF>") > -1)
-                        {
-                            break;
-                        }
-                    }
+                // An incoming connection needs to be processed.
+                //while (true)
+                //{
+                //    bytes = new byte[1024];
+                //    int bytesRec = handler.Receive(bytes);
+                //    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                //    if (data.IndexOf("<EOF>") > -1)
+                //    {
+                //        break;
+                //    }
+                //}
 
-                    // Show the data on the console.
-                    textBox1.Text = "Text received : {0}"+ data;
+                //// Show the data on the console.
+                //
 
-                    // Echo the data back to the client.
-                    byte[] msg = Encoding.ASCII.GetBytes(data);
+                //// Echo the data back to the client.
+                //byte[] msg = Encoding.ASCII.GetBytes(data);
 
-                    handler.Send(msg);
-                    handler.Shutdown(SocketShutdown.Both);
-                    handler.Close();
-                }
+                //handler.Send(msg);
+                //handler.Shutdown(SocketShutdown.Both);
+                //handler.Close();
+                //}
 
             }
             catch (Exception ev)
@@ -83,6 +90,19 @@ namespace socketWinServer
 
             Console.WriteLine("\nPress ENTER to continue...");
             Console.Read();
+        }
+
+        private void ClientSession(Socket clientSocket)
+        {
+            Console.WriteLine("\nClient Connected!!\n==================\n CLient IP {0}\n", clientSocket.RemoteEndPoint);
+            Byte[] bReceive = new Byte[1024 * 5000];
+            int i = clientSocket.Receive(bReceive);
+            textBox1.Text = "Recieved...";
+            string retorno = "";
+            for (int k = 0; k < i; k++)
+               retorno += Convert.ToChar(bReceive[k]);
+            textBox1.Text = "Text received : {0}" + retorno; ;
+            //throw new NotImplementedException();
         }
 
         private void Form1_Load(object sender, EventArgs e)
