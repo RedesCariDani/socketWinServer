dd56af15cf94360042a51b76ad509aeaef97fcda
Daniel Peralta Madriz <danielperaltamadriz@gmail.com>
2015-11-19 02:07:10 -0600

    commit

--- socketWinServer/Form1.Designer.cs
+++ socketWinServer/Form1.Designer.cs
@@ -32,49 +32,100 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.puerto = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Location = new System.Drawing.Point(13, 66);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
+            this.button1.TabIndex = 3;
             this.button1.Text = "Conectarse";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 47);
+            this.textBox1.Location = new System.Drawing.Point(13, 99);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(259, 151);
+            this.textBox1.Size = new System.Drawing.Size(153, 151);
             this.textBox1.TabIndex = 1;
             // 
             // puerto
             // 
-            this.puerto.Location = new System.Drawing.Point(172, 14);
+            this.puerto.Location = new System.Drawing.Point(57, 14);
             this.puerto.Name = "puerto";
             this.puerto.Size = new System.Drawing.Size(100, 20);
-            this.puerto.TabIndex = 2;
+            this.puerto.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(125, 17);
+            this.label1.Location = new System.Drawing.Point(10, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Puerto:";
             // 
+            // backgroundWorker1
+            // 
+           
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ip:";
+           
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(58, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(200, 99);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(153, 151);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(203, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Mensages Recibidos:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 212);
+            this.ClientSize = new System.Drawing.Size(461, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.puerto);
             this.Controls.Add(this.textBox1);
@@ -94,6 +145,11 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox puerto;
         private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- socketWinServer/Form1.cs
+++ socketWinServer/Form1.cs
@@ -10,12 +10,14 @@ using System.Windows.Forms;
 using System.Net.Sockets;
 using System.Net;
 using System.Threading;
+using System.IO;
 
 namespace socketWinServer
 {
     public partial class Form1 : Form
     {
         public static string data = null;
+        string destFolder = @"C:\ReceivedFiles";
         public Form1()
         {
             InitializeComponent();
@@ -23,91 +25,153 @@ namespace socketWinServer
 
         private void button1_Click(object sender, EventArgs e)
         {
-            byte[] bytes = new Byte[1024];
+            if (puerto.Text != "")
+            {
+                byte[] bytes = new Byte[1024];
 
 #pragma warning disable CS0618 // Type or member is obsolete
-                              // Establish the local endpoint for the socket.
-                              // Dns.GetHostName returns the name of the 
-                              // host running the application.
-            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());           
-            IPAddress ipAddress = ipHostInfo.AddressList[0];
-            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, int.Parse(puerto.Text));
-            //puerto.Text = "Conectándose a: " + localEndPoint.ToString();
+                // Establish the local endpoint for the socket.
+                // Dns.GetHostName returns the name of the 
+                // host running the application.
+                IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddress, int.Parse(puerto.Text));
+                //puerto.Text = "Conectándose a: " + localEndPoint.ToString();
+                this.SetText("Conectándose a: " + localEndPoint.ToString());
+                textBox1.Refresh();
 
-            // Create a TCP/IP socket.
-            Socket server = new Socket(AddressFamily.InterNetwork,
-                SocketType.Stream, ProtocolType.Tcp);
+                // Create a TCP/IP socket.
+                Socket server = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
 
-            // Bind the socket to the local endpoint and 
-            // listen for incoming connections.
-            try
-            {
-                server.Bind(localEndPoint);
-                server.Listen(10);
-
-                // Start listening for connections.
-                //while (true)
-                //{
-                    //textBox1.Text ="Waiting for a connection...";
-                    // Program is suspended while waiting for an incoming connection.
-                    Socket handler = server.Accept();
-                    data = null;
-                Console.WriteLine("Client has been accepted!");
-                var thread = new Thread(() => ClientSession(handler))
+                // Bind the socket to the local endpoint and 
+                // listen for incoming connections.
+                try
                 {
-                    IsBackground = true
-                };
-                thread.Start();
+                    server.Bind(localEndPoint);
+                    server.Listen(10);
 
-                // An incoming connection needs to be processed.
-                //while (true)
-                //{
-                //    bytes = new byte[1024];
-                //    int bytesRec = handler.Receive(bytes);
-                //    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                //    if (data.IndexOf("<EOF>") > -1)
-                //    {
-                //        break;
-                //    }
-                //}
+                    // Start listening for connections.
+                    while (true)
+                    {
+                        this.SetText("Waiting for a connection...");
+                        textBox1.Refresh();
+                        // Program is suspended while waiting for an incoming connection.
+                        Socket handler = server.Accept();
+                        data = null;
+                        this.SetText("\nClient Connected!!\n==================\n CLient IP\n" + handler.RemoteEndPoint + "\n");
+                        textBox1.Refresh();
 
-                //// Show the data on the console.
-                //
+                        /* CON trheads*/
+                        //var thread = new Thread(() => ClientSession(handler))
+                        //{
+                        //    IsBackground = true
+                        //};
+                        //thread.Start();
 
-                //// Echo the data back to the client.
-                //byte[] msg = Encoding.ASCII.GetBytes(data);
 
-                //handler.Send(msg);
-                //handler.Shutdown(SocketShutdown.Both);
-                //handler.Close();
-                //}
 
+
+
+                        /*El que sirve mejor*/
+
+                        //bytes = new byte[1024];
+                        //int bytesRec = handler.Receive(bytes);
+                        //data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                        //// Show the data on the console.
+                        //textBox3.Text += "Text received :" + data + "\n==================\n";
+                        //textBox3.Refresh();
+
+                        //// Echo the data back to the client.
+                        //byte[] msg = Encoding.ASCII.GetBytes("Msg recibido por el servidor");
+
+                        //handler.Send(msg);
+                        //handler.Shutdown(SocketShutdown.Both);
+                        //handler.Close();
+
+
+
+                        /*Con archivos*/
+
+                        byte[] clientData = new byte[1024 * 5000];
+                       // string receivedPath = "e:/";
+
+                        int receivedBytesLen = handler.Receive(clientData);
+                        int fileNameLen = BitConverter.ToInt32(clientData, 0);
+                        string fileName = "\\Recibido";
+                        Console.WriteLine("Client:{0} connected & File {1} started received.", handler.RemoteEndPoint, fileName);
+                        BinaryWriter bWrite = new BinaryWriter(File.Open(destFolder + fileName, FileMode.Append)); ;
+                        bWrite.Write(clientData);
+                        SetText("File: {0} received & saved at path: {1} " +fileName +" - "+ destFolder);
+                        textBox1.Refresh();
+
+
+                        /*Cierre de conexión*/
+                        // Echo the data back to the client.
+                        byte[] msg = Encoding.ASCII.GetBytes("Msg recibido por el servidor");
+
+                        handler.Send(msg);
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+
+                    }
+
+                }
+                catch (Exception ev)
+                {
+                    textBox1.Text = ev.ToString();
+                    textBox1.Refresh();
+                }
             }
-            catch (Exception ev)
-            {
-                textBox1.Text = ev.ToString();
-            }
-
-            Console.WriteLine("\nPress ENTER to continue...");
-            Console.Read();
+            else textBox1.Text = "Por favor digite primero un número de puerto";
+           
         }
 
         private void ClientSession(Socket clientSocket)
         {
-            Console.WriteLine("\nClient Connected!!\n==================\n CLient IP {0}\n", clientSocket.RemoteEndPoint);
+            //this.SetText("\nClient Connected!!\n==================\n CLient IP\n"+ clientSocket.RemoteEndPoint+"\n");
             Byte[] bReceive = new Byte[1024 * 5000];
             int i = clientSocket.Receive(bReceive);
-            textBox1.Text = "Recieved...";
+            //this.SetText( "Recieved...");
             string retorno = "";
             for (int k = 0; k < i; k++)
                retorno += Convert.ToChar(bReceive[k]);
-            textBox1.Text = "Text received : {0}" + retorno; ;
-            //throw new NotImplementedException();
+            textBox3.Text += "Text received :" + retorno + "\n==================\n";
+            textBox3.Refresh();
+
+            // Echo the data back to the client.
+            byte[] msg = Encoding.ASCII.GetBytes("Msg recibido por el servidor");
+
+            clientSocket.Send(msg);
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
         }
+
+        delegate void SetTextCallback(string text);
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.textBox1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBox1.Text += text+"\n";
+            }
+        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            textBox2.Text = ipAddress.ToString();
         }
+     
     }
 }
